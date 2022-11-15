0ee2876088d9671e5bbffd4a4914ae01496c9297
Andrew Kazbekov <ninjaxs2or@gmail.com>
2022-11-15 22:21:42 +0300

    App Navigation. Step #1
    Added navigation in the application by 2 fragments

---
+++ app/src/main/java/com/kazbekov/gyms/DetailFragment.kt
@@ -0,0 +1,13 @@
+package com.kazbekov.gyms
+
+import androidx.fragment.app.Fragment
+
+/**
+ *
+ * Shows detailed information about the workout.
+ * It is possible to change the data
+ *
+ * */
+
+class DetailFragment : Fragment() {
+}
---
+++ app/src/main/java/com/kazbekov/gyms/main/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.kazbekov.gyms.main
+
+import androidx.fragment.app.Fragment
+
+/**
+*
+* Contains a list of training days
+*
+* */
+
+class MainFragment : Fragment() {
+}
