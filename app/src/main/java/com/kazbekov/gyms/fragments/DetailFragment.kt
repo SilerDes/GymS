37bf768d1713a9dfceeeb085ceb987a8d733efc7
Andrew Kazbekov <ninjaxs2or@gmail.com>
2022-11-24 19:14:39 +0300

    DetailFragment Markup. Step #7

--- app/src/main/java/com/kazbekov/gyms/fragments/DetailFragment.kt
+++ app/src/main/java/com/kazbekov/gyms/fragments/DetailFragment.kt
@@ -1,7 +1,12 @@
 package com.kazbekov.gyms.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.kazbekov.gyms.R
+import com.kazbekov.gyms.databinding.FragmentDetailBinding
 
 /**
  *
@@ -11,4 +16,33 @@ import com.kazbekov.gyms.R
  * */
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private var isTrainingStateSaved = false
+    private val args: DetailFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentDetailBinding.bind(view)
+
+        initToolBar()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initToolBar() {
+        binding.toolbar.setNavigationOnClickListener {
+            if (!isTrainingStateSaved) saveTrainingState()
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun saveTrainingState() {
+
+    }
 }
